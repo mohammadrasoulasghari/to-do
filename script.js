36a4d0de7804f3b698243d55b043f7a1dfe12c1b
mohammadrasoul <mohammadrasoulasghari1@gmail.com>
2023-01-26 12:55:20 +0330

    build complet button and trash button and append ul
    q

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const todoInput =  document.querySelector(".todo-input");
 const todoButton =  document.querySelector(".todo-button");
-
+const todoList = document.querySelector(".todo-list")
 todoButton.addEventListener("click",addTodo);
 
 function addTodo(event){
   event.preventDefault();
   const todoDiv =document.createElement("div");
-  todoDiv.classList.add(".todo");
+  todoDiv.classList.add("todo");
   const todoLi=document.createElement("li");
   todoLi.innerText=todoInput.value;
-  console.log(todoLi)
+  todoLi.classList.add("todo-item"); 
+  todoDiv.appendChild(todoLi);
+  todoInput.value="";
+  console.log(todoLi);
+  const compleatButton = document.createElement("button");
+  compleatButton.innerHTML="<li class='fas fa-check'></li>";
+  compleatButton.classList.add("complete-btn")
+  todoDiv.appendChild(compleatButton);
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML="<li class='fas fa-trash'></li>";
+  trashButton.classList.add("complete-btn")
+  todoDiv.appendChild(trashButton);
+  todoList.appendChild(todoDiv)
+
 
 }
